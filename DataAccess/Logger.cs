6374e862543d587be0989823d404642bb1f44e29
tobi5648 <tobi5648@edu.campusvejle.dk>
2016-10-03 11:33:38 +0200

    Logger.cs was expanded

--- DataAccess/Logger.cs
+++ DataAccess/Logger.cs
@@ -5,24 +5,25 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Reflection;
     using System.Text;
     using System.Threading.Tasks;
 
     public class Logger
     {
-        private static readonly string Path;
+        private static readonly string exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         public static void Log (string message)
         {
             StreamWriter log;
 
-            if (!File.Exists("logfile.txt"))
+            if (!File.Exists(exePath + "\\" + "logfile.txt"))
             {
-                log = new StreamWriter("logfile.txt");
+                log = new StreamWriter(exePath + "\\" + "logfile.txt");
             }
             else
             {
-                log = File.AppendText("logfile.txt");
+                log = File.AppendText(exePath + "\\" + "logfile.txt");
             }
 
             log.WriteLine("Data Time: " + DateTime.Now);
