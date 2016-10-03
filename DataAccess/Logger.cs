4253fc0383adc865ef6ecd12317ad239dd67ca24
tobi5648 <tobi5648@edu.campusvejle.dk>
2016-10-03 11:13:08 +0200

    Added a logger

---
+++ DataAccess/Logger.cs
@@ -0,0 +1,42 @@
+﻿namespace DataAccess
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Logger
+    {
+        private static readonly string Path;
+
+        public static void Log (string message)
+        {
+            StreamWriter log;
+
+            if (!File.Exists("logfile.txt"))
+            {
+                log = new StreamWriter("logfile.txt");
+            }
+            else
+            {
+                log = File.AppendText("logfile.txt");
+            }
+
+            log.WriteLine("Data Time: " + DateTime.Now);
+
+            if (message.Contains("logged"))
+            {
+                log.WriteLine("CLient: " + message + " has logged on");
+            }
+            if (message.Contains("Exception"))
+            {
+                log.WriteLine("Exception Name: " + message);
+            }
+
+            log.Close();
+        }
+    }
+}
